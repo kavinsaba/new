56bea0ff4a1e9f45594b4835dcac29d288b1d4fa
kavinsaba <kavinsaba14@gmail.com>
2021-08-11 01:57:11 +0530

    ghk

--- src/test/java/org/tesyt/TestGid.java
+++ src/test/java/org/tesyt/TestGid.java
@@ -6,36 +6,43 @@ import org.testng.annotations.Test;
 
 public class TestGid {
 
-@Test
-public void mco1() {
-System.out.println("Method1");
-}
-@Test
-public void mc02() {
-System.out.println("Method2");
-}
-@Test
-public void mc03() {
-System.out.println("Method3");
-}
-@Test
-public void mc04() {
-System.out.println("Method4");
-}
-@Test
-public void mc05() {
-System.out.println("Method5");
-}
-@Test 
-public void mc06() {
-System.out.println("Method6");
-}
-@Test
-public void rc07() {
-System.out.println("Method7");
-}
-@Test
-public void rc08() {
-System.out.println("Method8");
-}
+	@Test
+	public void mco1() {
+		System.out.println("Method1");
+	}
+
+	@Test
+	public void mc02() {
+		System.out.println("Method2");
+	}
+
+	@Test
+	public void mc03() {
+		System.out.println("Method3");
+	}
+
+	@Test
+	public void mc04() {
+		System.out.println("Method4");
+	}
+
+	@Test
+	public void mc05() {
+		System.out.println("Method5");
+	}
+
+	@Test
+	public void mc06() {
+		System.out.println("Method6");
+	}
+
+	@Test
+	public void rc07() {
+		System.out.println("Method7");
+	}
+
+	@Test
+	public void rc08() {
+		System.out.println("Method8");
+	}
 }
