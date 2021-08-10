8443025cc83f04cfd98a1aed1501f7704da39a25
kavinsaba <kavinsaba14@gmail.com>
2021-08-11 00:52:16 +0530

    new

---
+++ src/test/java/org/tesyt/TestGid.java
@@ -0,0 +1,33 @@
+package org.tesyt;
+
+import java.lang.reflect.Method;
+
+import org.testng.annotations.Test;
+
+public class TestGid {
+
+@Test
+public void mco1() {
+System.out.println("Method1");
+}
+@Test
+public void mc02() {
+System.out.println("Method2");
+}
+@Test
+public void mc03() {
+System.out.println("Method3");
+}
+@Test
+public void mc04() {
+System.out.println("Method4");
+}
+@Test
+public void mc05() {
+System.out.println("Method5");
+}
+@Test 
+public void mc06() {
+System.out.println("Method6");
+}
+}
